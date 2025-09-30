2db052bc8b53f3ecae1f45c2ceed9f8198e8db60
john125892 <johnsteven747474@gmail.com>
2025-09-30 09:48:13 -0500

    e

--- js/libro.js
+++ js/libro.js
@@ -1,9 +1,15 @@
- export default class Libro {
-    constructor(titulo, autor, editorial,idioma) {
+let libros=[];
+
+
+export default class agregarLibro {
+    constructor(titulo, autor, editorial, idioma) {
         this.titulo = titulo;
         this.autor = autor;
         this.editorial = editorial;
         this.idioma = idioma;
     }
 
+    mostrarInfo() {
+        return `${this.titulo} -${this.autor} ${this.editorial} ${this.idioma}`
+    }
 }
